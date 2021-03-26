28144aeb5b59bc6a476cbbdf95be47b02e0cc390
Prashanth Palki <prashanthpalki1999@gmail.com>
2021-03-26 12:14:42 +0530

    Added new Project

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full Stack Developer","Designer"],
+        strings: ["Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer","Designer"],
+        strings: ["Full Stack Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
